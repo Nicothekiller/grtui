d391ba2c92053c459718e5987c035f43f793f5c5
Nicothekiller <nicolasnaran@gmail.com>
2025-02-27 15:35:52 -0500

    set up initial layout

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,26 @@
 use ratatui::{
+    layout::{Constraint, Layout},
     style::Stylize,
-    text::Line,
+    text::{Line, Span},
     widgets::{Block, Paragraph},
     Frame,
 };
 
 /// Draw fn to use with ratatui. Draws the events happening on the screen.
 pub fn draw(frame: &mut Frame) {
-    let text = Line::raw("Linea 1");
-    let magia = Line::raw("Linea 2").red();
+    let horizontal = Layout::horizontal([Constraint::Length(30), Constraint::Fill(1)]);
+    let [semester_area, info_area] = horizontal.areas(frame.area());
 
-    let par = Paragraph::new(vec![text, magia]).block(Block::bordered());
+    let semester_block = Block::bordered().title(Span::from("Semestres").green());
+    let info_block = Block::bordered().title("Info");
 
-    frame.render_widget(par, frame.area());
+    let s_lines: Vec<Line> = vec![
+        Line::from("Semestre 1").green(),
+        Line::from("Semestre 2").red(),
+        Line::from("Semestre 3").yellow(),
+    ];
+    let semesters = Paragraph::new(s_lines).block(semester_block);
+
+    frame.render_widget(semesters, semester_area);
+    frame.render_widget(info_block, info_area);
 }
